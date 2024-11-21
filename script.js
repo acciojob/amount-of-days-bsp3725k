426fc1189a800cf0ec9f64a05ef9a9964a3a3e4f
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-21 07:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return 366; // Leap year
+    } 
+	else {
+        return 365; // Non-leap year
+    }
+}
+
+let year = prompt("Enter the to how many days in that year");
+alert(daysOfYear(year));
